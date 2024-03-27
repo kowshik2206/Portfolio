233d02efd9a30e695297b58ff8109dab0dc76470
Kowshik Sai <89956274+kowshik2206@users.noreply.github.com>
2024-03-27 13:58:37 +0530

    Add files via upload

---
+++ Portfolio website/script.js
@@ -0,0 +1,7 @@
+function downloadResume() {
+    window.location.href = 'kowshik_resume.pdf'; // Replace with the actual path to your resume file
+}
+
+function openLinkedIn() {
+    window.open('https://www.linkedin.com/in/eletem-kowshik-sai/', '_blank');
+}
